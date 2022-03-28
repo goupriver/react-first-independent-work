68ba53a45db32abb44bdde735d161b9fc3435096
Alexander Mironov <goupriver@gmail.com>
2022-03-28 23:13:02 +0700

    feat(ListFolder): add animation-transition

--- src/components/ListFolder.jsx
+++ src/components/ListFolder.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import Folder from "./Folder";
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 
 const SetupTask = styled.div`
   margin-bottom: 20px;
@@ -14,18 +18,33 @@ const ListFolder = ({
 }) => {
   return (
     <SetupTask>
-      <ul>
-        {folders &&
-          folders.map((folder) => (
-            <Folder
-              isActiveFolder={isActiveFolder}
-              setIsActiveFolder={setIsActiveFolder}
-              key={folder.folderId}
-              folder={folder}
-              removeFolder={removeFolder}
-            />
-          ))}
-      </ul>
+      {folders &&
+        <ul>
+          <TransitionGroup>
+            {folders.map((folder) => (
+              <CSSTransition
+                key={folder.folderId}
+                timeout={{
+                  appear: 700,
+                  enter: 1000,
+                  exit: 700,
+                 }}
+                classNames={{
+                  enterActive: 'animate__animated animate__lightSpeedInLeft',
+                  exitActive: 'animate__animated animate__lightSpeedOutLeft',
+                 }}
+              >
+                <Folder
+                  isActiveFolder={isActiveFolder}
+                  setIsActiveFolder={setIsActiveFolder}
+                  folder={folder}
+                  removeFolder={removeFolder}
+                />
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ul>
+      }
     </SetupTask>
   );
 };
